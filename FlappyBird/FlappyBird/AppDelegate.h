2541f37be37e958194bd3e4edd5730f59d94a5c2
Gwyneth94143 <m15801725054@163.com>
2017-08-04 13:31:07 +0800

    初始化

---
+++ FlappyBird/FlappyBird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlappyBird
+//
+//  Created by Gwyneth Gan on 04/08/2017.
+//  Copyright © 2017 Gwyneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlappyBird/FlappyBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlappyBird
+//
+//  Created by Gwyneth Gan on 04/08/2017.
+//  Copyright © 2017 Gwyneth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
